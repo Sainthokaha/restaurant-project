3f747be08eb7cb6e1a0d2c544700e0f4b14edc3c
sainthokaha5@gmail.com <88292276+Sainthokaha@users.noreply.github.com>
2023-08-06 21:39:12 +0100

    second commit

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,3 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/RESTUARANT-PROJECT/'
-    : '/'
-}
